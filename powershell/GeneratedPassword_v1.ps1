03b4c5904aac5859a8582ff69528edd19684adc8
laszlon <nemethl91@hotmail.com>
2022-03-04 17:51:12 +0100

    PS - Random Passwd

---
+++ powershell/GeneratedPassword_v1.ps1
@@ -0,0 +1,29 @@
+﻿<#
+SCOPE
+        User által megadott hosszúságú jelszó generáló script. Elfogadható karakterkészlet módosítható.
+#>
+
+$length = Read-Host "Add meg, milyen hosszú legyen a jelszó!"
+
+
+function Get-RandomPassword {
+    param (
+        [Parameter(Mandatory)]
+        [int] $length
+    )
+    #$charSet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789{]+-[*=@:)}$^%;(_!&amp;#?>/|.'.ToCharArray() --> karakterkészlet lehet bővebb is
+    $charSet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'.ToCharArray()
+    $pass = New-Object System.Security.Cryptography.RNGCryptoServiceProvider
+    $bytes = New-Object byte[]($length)
+ 
+    $pass.GetBytes($bytes)
+ 
+    $result = New-Object char[]($length)
+ 
+    for ($i = 0 ; $i -lt $length ; $i++) {
+        $result[$i] = $charSet[$bytes[$i]%$charSet.Length]
+    }
+ 
+    return (-join $result)
+}
+Get-RandomPassword $length
